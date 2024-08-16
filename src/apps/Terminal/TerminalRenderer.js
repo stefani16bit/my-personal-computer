88fd80467014b240d547f457866bee28a3e86fd6
Stefani Marchi <99232385+stefanimarchi@users.noreply.github.com>
2024-08-15 22:34:42 -0300

    terminal remake

--- src/apps/Terminal/TerminalRenderer.js
+++ src/apps/Terminal/TerminalRenderer.js
@@ -2,87 +2,30 @@ import React, { useState, useEffect, useRef } from "react";
 
 import "./TerminalRenderer.css";
 
+import commands from "./terminal-commands.json";
+
 function TerminalRenderer({ appCoreRef }) {
-	const [terminalLog, setTerminalLog] = useState(["type /ask for help"]);
-	const [currentCommand, setCurrentCommand] = useState("");
-	const [mode, setMode] = useState("initial");
-	const inputRef = useRef(null);
+	const [terminalLog, setTerminalLog] = useState([]);
 
 	useEffect(() => {
-		setTerminalLog(["type /ask for help"]);
+		setTerminalLog(["type /help to see all commands avaliable."]);
 	}, []);
 
 	useEffect(() => {
-		if (inputRef.current) {
-			inputRef.current.focus();
-		}
+		function handleInputChange(event) {}
+		
+		window.addEventListener("keydown", handleInputChange);
+
+		return () => {
+			window.removeEventListener("keydown", handleInputChange);
+		};
 	}, []);
-
-	const handleInputChange = (event) => {
-		setCurrentCommand(event.target.value);
-	};
-
-	const handleCommandSubmit = () => {
-		const command = currentCommand.trim();
-		setCurrentCommand("");
-
-		if (command === "/ask") {
-			processAskCommand();
-		} else if (command === "cls") {
-			clearTerminal();
-		} else if (mode === "asking") {
-			processOptionSelection(command);
-		} else {
-			setTerminalLog([...terminalLog, `Unknown command: ${command}`]);
-		}
-	};
-
-	const clearTerminal = () => {
-		setTerminalLog(["type /ask for help"]);
-		setMode("initial");
-	};
-
-	const processAskCommand = () => {
-		setTerminalLog([...terminalLog, "(1) How old are you?", "(2) When you started programming?", "(3) What's your sign and MBTI?"]);
-		setMode("asking");
-	};
-
-	const processOptionSelection = (option) => {
-		let response;
-		switch (option) {
-			case "1":
-				response = "I'm currently 22.";
-				break;
-			case "2":
-				response = "I have started programming in 2022.";
-				break;
-			case "3":
-				response = "Cancer and INFJ.";
-				break;
-			default:
-				response = "Invalid option";
-		}
-		setTerminalLog([...terminalLog, ` ${option}: ${response}`]);
-		setMode("initial");
-	};
 
 	return (
 		<div className="terminal-renderer-container">
 			{terminalLog.map((line, index) => (
 				<div key={index}>{line}</div>
 			))}
-			<input
-				type="text"
-				value={currentCommand}
-				onChange={handleInputChange}
-				onKeyPress={(e) => {
-					if (e.key === "Enter") {
-						handleCommandSubmit();
-					}
-				}}
-				className="terminal-input"
-				ref={inputRef}
-			/>
 		</div>
 	);
 }
