dc29569bea06227e340090db0a1c9c7a2feaecd3
Stefani <822145016@ulife.com.br>
2024-11-20 13:24:49 -0300

    feat: clippy component functionalities

--- src/components/Clippy.js
+++ src/components/Clippy.js
@@ -4,13 +4,35 @@ import { useState, useEffect } from "react";
 
 function Clippy() {
 	const [isClippyOpened, setIsClippyOpened] = useState(false);
+	const [clippyText, setClippyText] = useState("Hello, piece of human flesh.");
+
+	const textSequences = [
+		["oiii", 2],
+		["ti amo mozi", 7],
+		["vc quer namora comigo?", 9],
+	];
 
 	useEffect(() => {
-		if (document.visibilityState === "visible") {
-			const timer = setTimeout(() => {
-				setIsClippyOpened(true);
-			}, 5000);
-		}
+		const textTimeoutId = setTimeout(() => {
+			setIsClippyOpened(true);
+
+			for (const element of textSequences) {
+				const text = element[0];
+				const time = element[1];
+				setTimeout(() => {
+					setClippyText(text);
+				}, time * 1000);
+			}
+		}, 1000);
+
+		const hideClippyTimeoutId = setTimeout(() => {
+			setIsClippyOpened(false);
+		}, 30000);
+
+		return () => {
+			clearTimeout(textTimeoutId);
+			clearTimeout(hideClippyTimeoutId);
+		};
 	}, []);
 
 	return (
@@ -18,7 +40,7 @@ function Clippy() {
 			{isClippyOpened && (
 				<div className="clippy-container">
 					<div className="clippy-balloon">
-						<a className="clippy-balloon-text">Hello, piece of human flesh.</a>
+						<a className="clippy-balloon-text">{clippyText}</a>
 					</div>
 					<div className="clippy-balloon-arrow" />
 					<div className="clippy-img">
