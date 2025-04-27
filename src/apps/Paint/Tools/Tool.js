751fe8b64c8e230712758e95222ce2427c0cfe38
Stefani <822145016@ulife.com.br>
2025-04-27 14:54:16 -0300

    add tool interface

--- src/apps/Paint/Tools/Tool.js
+++ src/apps/Paint/Tools/Tool.js
@@ -2,16 +2,14 @@ class Tool {
 	constructor(paintRendererRef) {
 		this.paintRendererRef = paintRendererRef;
 	}
-    
+
 	onMouse1Click(event) {}
-
 	onMouse2Click(event) {}
-
 	onMousePressBegin(event) {}
-
 	onMousePressLeave(event) {}
-
 	onMouseMove(event) {}
+	onToolActivated() {}
+	onToolDisabled() {}
 }
 
 export default Tool;
